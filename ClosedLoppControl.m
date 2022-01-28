39131defee5819534e874f2f3536dc14487d2c39
Azadeh Doroudchi <54753659+adoroud@users.noreply.github.com>
2022-01-28 14:55:19 -0700

    Update ClosedLoppControl.m
    01/28/2022 added gravity effect

--- ClosedLoppControl.m
+++ ClosedLoppControl.m
@@ -16,7 +16,7 @@ r1 = da*(cos(pi/4))*(e1+e2);  %actuator 1 distance vector to backbone [m]
 r2 = da*(sqrt(2)/2)*(e1-e2); %actuator 2 distance vector to backbone [m]
 r3 = da*(sqrt(2)/2)*(-e1-e2); %actuator 3 distance vector to backbone [m]
 r4 = da*(sqrt(2)/2)*(-e1+e2); %actuator 4 distance vector to backbone [m]
-db = (L0/2)*e3; %distance of the tip force (FSR sensor) to the center of mass vector [m]
+db = L0*e3; %distance of the tip force (FSR sensor) to the center of mass vector [m]
 A0 = pi*r0^2; % cicular cross-sectional area [m^2]
 rho = 792; % density (kg/m^3)
 M = rho*A0*J; %material cross-section stiffness per unit length [Nm]
@@ -26,17 +26,19 @@ Ixx = (pi/4)*r0^4;  % second moment of inertai for circular cross-section  %rect
 Iyy = (pi/4)*r0^4;  % second moment of inertai for circular cross-section circular   %rectangular: (width*height^3)/12;
 Izz=Ixx+Iyy;
 J = diag([Ixx  Iyy  Izz]); % Second mass moment of inertia tensor
-Kb = diag([E*J(1,1), E*J(2,2), G*J(3,3)]); % Stiffness matrix for bending(d1,d2) and extension(d3) [N.m^2]
+Kb = diag([E*J(1,1), E*J(2,2), G*J(3,3)]); % Stiffness matrix for bending(d1,d2) and torsion (d3) [N.m^2]
+% Ke = diag([G*A0, G*A0, E*A0]); % Stiffness matrix for shear (d1,d2) and extension(d3) [N]
 tau = 0; %time period of free-end damping in x-y-z axes [s]
 Bb = Kb* diag([tau, tau, tau]); % Damping matrix corresponding to bending and extension [N.m^2.s]
 Force_tip = -e1; %zeros(1,3); %Force from interaction with environment (FSR) [N]
 Moment_tip = cross(db,Force_tip); %Moment from interaction with the environment (FSR) [Nm]
-g = [0 ; 0 ; -9.81] ;  %gravity accelaration [m/s^2]
-Force_gravity = rho*A0*g; %force per unit length at each cross-section [N]
+g = -9.81*e3 ;  %gravity accelaration [m/s^2]
+Force_gravity = rho*A0*g; %force per unit length at each cross-section [N/m]
 % u =[ux;uy;uz]; The curvature vector describing bending along x-y axes [1/m]
 u_star = [0;0;0]; %undeformed state of u [1/m]
-a = 1;  %amplitude of bending limited by the design and worksapce
-freq = 1; %frequency of bending limited by air pressure speed [Hz]
+% v_star = [0;0;1]; %undeformed state of v [-]
+a = 1;  %amplitude of bending limited by the design and worksapce range=(0,6) [1/m]
+freq = 1; %frequency of bending limited by air pressure speed [Hz] range=
 w = 2*pi*freq;  
 %% The current code is written for one segment, based on the backbone passsing through each cross section
 for i=1:STEPS
@@ -58,7 +60,9 @@ for i=1:STEPS
 
     u_y = 2*(x(i)-x0)/((x(i)-x0)^2+(z(i)-z0)^2);  %bending curvature along x-axis
     u = [0; u_y; 0];  %bending curvature vector
-
+    theta = asin(u_y*(z(i)-z0));
+    x_c = (1-cos(theta/2)/u_y);
+    Moment_gravity = cross((x_c-x0)*e1,Force_gravity);  %moment per unit length at each cross-section [N/m]
 %     xt(i) = (x-x_previous_step)/dt;
 %     yt(i) = (y-y_previous_step)/dt;
 %     zt(i) = (z-z_previous_step)/dt;
@@ -69,9 +73,9 @@ for i=1:STEPS
 %     D= Bb; %Derivative coefficient matrix
     torq_l_desired = K*(u_desired-u_star); %+D*ut_desired; %desired torque along x-axis on the backbone
 
-    applied_torque_by_actuator= R*(torq_l_desired-torq_l+M*utt_desired-Moment_tip); %[Nm], 3x1 vector
-    applied_force_by_actuator_1= applied_torque_by_actuator(2)/(2*da*cos(pi/4));  %[N], scalar
-    applied_force_by_actuator_2= applied_torque_by_actuator(2)/(2*da*cos(pi/4)); %[N]
+    applied_torque_by_actuator= R*(torq_l_desired-torq_l+M*utt_desired-Moment_tip-Moment_gravity); %[Nm], 3x1 vector
+    applied_force_by_actuator_1= abs(applied_torque_by_actuator(2))/(2*da*cos(pi/4));  %[N], scalar
+    applied_force_by_actuator_2= abs(applied_torque_by_actuator(2))/(2*da*cos(pi/4)); %[N]
     applied_pressure_by_actuator_1= applied_force_by_actuator_1/A0; %[N/m^2]=[Pa]
     applied_pressure_by_actuator_2= applied_force_by_actuator_2/A0; %[N/m^2]=[Pa]
 end
